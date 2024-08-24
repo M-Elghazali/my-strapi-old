fdb1b6bece02a729495dd9eaf4cd179ec7feed99
wazerco <alfreda754@yogirt.com>
2024-08-24 18:48:07 +0300

    this is my starpi deply

---
+++ config/plugins.js
@@ -0,0 +1,37 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
+    email: {
+        config: {
+            provider: 'nodemailer',
+            providerOptions: {
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
+                auth: {
+                    user: env('SMTP_USER'),
+                    pass: env('SMTP_PASS'),
+                },
+            },
+            settings: {
+                defaultFrom: 'no-reply@example.com',
+                defaultReplyTo: 'no-reply@example.com',
+            },
+        },
+    },
+    // ...
+});
